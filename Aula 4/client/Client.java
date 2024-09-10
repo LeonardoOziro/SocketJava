3790bf47dcbf27db6c88407acf46767d274ae5d8
Leonardo <144481202+LeonardoOziro@users.noreply.github.com>
2024-09-10 11:23:16 -0300

    Aula

---
+++ Aula 4/client/Client.java
@@ -0,0 +1,41 @@
+//aula 4 (10/09/24)
+
+package client;
+
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+        final String IP = "127.0.0.1";
+        final int PORT = 9876;
+        PrintStream printStream = null;
+
+        //criação do socket e pedido de conexão
+        try {
+            socket = new Socket(IP, PORT);
+            System.out.println("Conectado ao servidor");
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar com o servidor");
+            System.out.println(e.getMessage());
+        }
+
+        //comunicação
+        try {
+            printStream = new PrintStream(socket.getOutputStream());
+            printStream.println("Olá DANILO.");
+        } catch (Exception e) {
+            System.out.println("Erro na comunicação " + e.getMessage());
+        }
+
+        //desconexão
+        try {
+            System.out.println("Encerrando o cliente");
+            socket.close();
+        } catch (Exception e) {
+            
+        }
+    }
+    
+}
